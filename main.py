30e88b9e8100a4f7ea3bb60d88bf10bda3f8c808
Vaibhav <vaihbav.shrivastava@antiersolution.com>
2025-09-22 11:28:05 +0530

    photo size

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ uploaded_file = st.file_uploader("Upload an image:", type=["jpg", "png", "jpeg"]
 
 if uploaded_file:
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_container_width=True)
+    with st.container(width=300):
+        st.image(image, caption="Uploaded Image", use_container_width=True)
 
     image = image.resize((640, 640))
     image_array = np.expand_dims(image, axis=0)
