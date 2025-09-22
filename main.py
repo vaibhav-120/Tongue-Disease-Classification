608a0fbf45afabca548c33e53a4a0b77b2ff316e
Vaibhav <vaihbav.shrivastava@antiersolution.com>
2025-09-22 11:18:06 +0530

    done

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import streamlit as st
 from PIL import Image
 import numpy as np
+import os
 from tensorflow.keras.models import load_model
 
 st.set_page_config(page_title="", layout="wide")
 
 model = load_model("model2.keras")
-model.summary()
 
 
 CLASS_NAMES = ["Diabetes","Healthy"]
